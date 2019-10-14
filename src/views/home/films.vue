85dce51317c4363c8ba31d8bfa9a33b645961c16
Qu Daifeng <2500921028qq@.com>
2019-10-14 22:20:28 +0800

    电影列表头部

--- src/views/home/films.vue
+++ src/views/home/films.vue
@@ -10,7 +10,7 @@
 
             <div class="city i-logo">
                 <div>北京</div>            
-                <span></span>             
+                <span class="arrow"></span>             
             </div>
 
             <!-- Tabt 切换 -->
@@ -22,15 +22,19 @@
                 </ul>
             </div>
         </div>
+
     </div>
 </template>
+
 
 <style lang="scss">
     .page-home {
         
+        
+
         .head {
-            height: 42px;
-            
+            height: 50px;
+           display: flex;
 
             .i-logo{ 
                 background-image: url(//gw.alicdn.com/tfs/TB1mKkzl9zqK1RjSZFpXXakSXXa-50-50.svg);
@@ -41,19 +45,45 @@
             }
             .city{
                 width: 85px;
-                line-height: 42px;
-                display: inline-block;
+                line-height: 50px;
+                
 
                 div{
                     display: inline-block;
                 }
+
+                .arrow:after {
+                    content: "";
+                    top: 50%;
+                    left: 50%;
+                    width: 0;
+                    height: 0;
+                    border-left: 4px solid transparent;
+                    border-right: 4px solid transparent;
+                    border-top: 4px solid #333;
+                    display: block;
+                    position: absolute;
+                    margin: -1px 0 0 -4px;
+                    -webkit-tap-highlight-color: rgba(0,0,0,0);
+                    box-sizing: border-box;
+                }
             }
         }
         .tablist{
-            display: inline-block;
+           
+            
 
-            li{
+            ul{
+                display: flex;
+                justify-content: space-between;
+               
 
+                li{
+                    height: 50px; 
+                    line-height: 50px;
+                    padding: 0 15px;
+                    color: #777;       
+                }
             }
         }
     }
