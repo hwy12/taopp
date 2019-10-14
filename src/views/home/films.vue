53cba1153a9b3960a285deabb1a394aadc0ce03d
Qu Daifeng <2500921028qq@.com>
2019-10-14 21:03:21 +0800

    电影列表头部

--- src/views/home/films.vue
+++ src/views/home/films.vue
@@ -1,6 +1,60 @@
 <template>
     <div class="page-home">
         <!-- 二级路由，影片列表 -->
-        <h1>影片列表</h1>
+
+         <!-- head 头部 -->
+
+        <div class="head">
+
+            <!-- city 城市 -->
+
+            <div class="city i-logo">
+                <div>北京</div>            
+                <span></span>             
+            </div>
+
+            <!-- Tabt 切换 -->
+        
+            <div class="tablist">
+                <ul>
+                    <li>正在热映</li>
+                    <li>即将上映</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
+
+<style lang="scss">
+    .page-home {
+        
+        .head {
+            height: 42px;
+            
+
+            .i-logo{ 
+                background-image: url(//gw.alicdn.com/tfs/TB1mKkzl9zqK1RjSZFpXXakSXXa-50-50.svg);
+                background-size: 25px 25px;
+                background-repeat: no-repeat;
+                background-position: 15px;
+                padding-left: 50px;
+            }
+            .city{
+                width: 85px;
+                line-height: 42px;
+                display: inline-block;
+
+                div{
+                    display: inline-block;
+                }
+            }
+        }
+        .tablist{
+            display: inline-block;
+
+            li{
+
+            }
+        }
+    }
+</style>
--- src/views/movie/index.vue
+++ src/views/movie/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="page-movie"></div>
-    <!-- 一级路由，电影影院选择列表 -->
-    <h1>电影影院选择列表</h1>
+    <div class="page-movie">
+         <!-- 一级路由，电影影院选择列表 -->
+         <h1>电影影院选择列表</h1>
+    </div>
 </template>
